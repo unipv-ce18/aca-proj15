5d54ddf8488e3180d2f0bc37262503e383f7dd71
Marco <marco.prina01@universitadipavia.it>
2019-01-19 20:49:52 +0100

    Legge qualsiasi cosa, basta che i float abbiano punti e non virgole, nel main bisogna specificare il numero di in e di out, gli out devono essere in ultima posizione

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include "seriale.h"
 #include "TestOpenMP.h"
+#include "ReadFile.h"
+
 
 int main() {
-    //seriale();
-    testOMP();
+    int numIn=2, numOut=1, patternsnumPat;
+
+    struct data *allData=readData(numIn, numOut, &patternsnumPat);
+
+    seriale(allData, numIn, numOut, patternsnumPat);
+
     return 0;
 }
