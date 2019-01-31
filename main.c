c646d6471b39e08cd8bba51501190633d081f017
StefanoRicci94 <mr.ricci@hotmail.it>
2019-01-31 12:34:05 +0100

    Remove OpenMp

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "seriale.h"
-#include "TestOpenMP.h"
 #include "ReadFile.h"
 
 
 int main() {
-    int numIn=11, numOut=1, numPat;
+    int numIn=2, numOut=1, numPat;
 
     struct data *allData=readData(numIn, numOut, &numPat);
 
