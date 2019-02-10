3c6c96e039b6fb317c0f34916e9195cd8dae296c
Marco <marco.prina01@universitadipavia.it>
2019-02-10 10:20:36 +0100

    Preso codice da seriale e messo per il parallelo

--- parallel.c
+++ parallel.c
@@ -5,129 +5,163 @@
 #include <fcntl.h>
 #include <omp.h>
 #include "parallel.h"
+#include "readData.h"
+#include "readInitialWeight.h"
 
-#define NUMPAT 4
-#define NUMIN  2
-#define NUMHID 2
-#define NUMOUT 1
 
-#define rando() ((double)rand()/((double)RAND_MAX+1))
+#define NUMHID 7
 
-int parallel() {
-    printf("prova");
-    int    i, j, k, p, np, op, ranpat[NUMPAT+1], epoch;
-    int    NumPattern = NUMPAT, NumInput = NUMIN, NumHidden = NUMHID, NumOutput = NUMOUT;
-    double Input[NUMPAT+1][NUMIN+1] = { 0, 0, 0,  0, 1, 1,  0, 1, 0,  0, 0, 1,  0, 0, 0 };
-    double Target[NUMPAT+1][NUMOUT+1] = { 0, 0,  0, 1,  0, 0,  0, 0,  0, 1 };
-    double SumH[NUMPAT+1][NUMHID+1], WeightIH[NUMIN+1][NUMHID+1], Hidden[NUMPAT+1][NUMHID+1];
-    double SumO[NUMPAT+1][NUMOUT+1], WeightHO[NUMHID+1][NUMOUT+1], Output[NUMPAT+1][NUMOUT+1];
-    double DeltaO[NUMOUT+1], SumDOW[NUMHID+1], DeltaH[NUMHID+1];
-    double DeltaWeightIH[NUMIN+1][NUMHID+1], DeltaWeightHO[NUMHID+1][NUMOUT+1];
-    double Error, eta = 0.5, alpha = 0.9, smallwt = 0.5;
+#define rando() (((double)rand()/((double)RAND_MAX+1)))
 
-//#pragma omp for schedule reduction (+: DeltaWeightIH, WeightIH )
-    for( j = 1 ; j <= NumHidden ; j++ ) {    /* initialize WeightIH and DeltaWeightIH  */
-//#pragma omp for schedule reduction (+: DeltaWeightIH, WeightIH)
-        for( i = 0 ; i <= NumInput ; i++ ) {
+int parallel(struct data * allData, int numIn, int numOut, int numPattern) {
+
+    int    i, j, k, p, np, op, ranpat[numPattern+1], epoch;
+    int    NumHidden = NUMHID;
+    double SumH[numPattern+1][NUMHID+1], **WeightIH, Hidden[numPattern+1][NUMHID+1];
+    double SumO[numPattern+1][numOut+1], **WeightHO, Output[numPattern+1][numOut+1];
+    double DeltaO[numOut+1], SumDOW[NUMHID+1], DeltaH[NUMHID+1];
+    double DeltaWeightIH[numIn+1][NUMHID+1], DeltaWeightHO[NUMHID+1][numOut+1];
+    double Error, eta = 0.02, alpha = 0.1;
+    double accuracy=0, minAccuracy=10.0;
+    double sensitivity=0, maxSensitivity=0;
+
+
+    WeightIH=readInitialWeightIH(numIn, NumHidden);
+
+    for( j = 1 ; j <= NumHidden ; j++ ) {
+        for( i = 0 ; i <= numIn ; i++ ) {
             DeltaWeightIH[i][j] = 0.0 ;
-            WeightIH[i][j] = 2.0 * ( rando() - 0.5 ) * smallwt ;
         }
     }
 
-//#pragma omp for schedule reduction (+: DeltaWeightHO, WeightHO )
-    for( k = 1 ; k <= NumOutput ; k ++ ) {    /* initialize WeightHO and DeltaWeightHO */
-//#pragma omp for schedule reduction (+: DeltaWeightHO, WeightHO)
+    WeightHO= readInitialWeightHO(NumHidden, numOut);
+    for( k = 1 ; k <= numOut ; k ++ ) {
         for( j = 0 ; j <= NumHidden ; j++ ) {
             DeltaWeightHO[j][k] = 0.0 ;
-            WeightHO[j][k] = 2.0 * ( rando() - 0.5 ) * smallwt ;
         }
     }
 
-
-    for( epoch = 0 ; epoch < 10000000 ; epoch++) {    /* iterate weight updates */
-        for( p = 1 ; p <= NumPattern ; p++ ) {    /* randomize order of training patterns */
+    for( epoch = 0 ; epoch < 5000 ; epoch++) {    /* iterate weight updates */
+        for( p = 1 ; p <= numPattern ; p++ ) {    /* randomize order of training patterns */
             ranpat[p] = p ;
         }
-        for( p = 1 ; p <= NumPattern ; p++) {
-            np = p + rando() * ( NumPattern + 1 - p ) ;
+        for( p = 1 ; p <= numPattern ; p++) {
+            np = (int) (p + rando() * (numPattern + 1 - p ));
             op = ranpat[p] ; ranpat[p] = ranpat[np] ; ranpat[np] = op ;
         }
         Error = 0.0 ;
-        for( np = 1 ; np <= NumPattern ; np++ ) {    /* repeat for all the training patterns */
+        for( np = 1 ; np <= numPattern ; np++ ) {    /* repeat for all the training patterns */
             p = ranpat[np];
             for( j = 1 ; j <= NumHidden ; j++ ) {    /* compute hidden unit activations */
                 SumH[p][j] = WeightIH[0][j] ;
-                for( i = 1 ; i <= NumInput ; i++ ) {
-                    SumH[p][j] += Input[p][i] * WeightIH[i][j] ;
+                for( i = 1 ; i <= numIn ; i++ ) {
+                    SumH[p][j] += allData[p].in[i] * WeightIH[i][j] ;
                 }
                 Hidden[p][j] = 1.0/(1.0 + exp(-SumH[p][j])) ;
             }
-            for( k = 1 ; k <= NumOutput ; k++ ) {    /* compute output unit activations and errors */
+            for( k = 1 ; k <= numOut ; k++ ) {    /* compute output unit activations and errors */
                 SumO[p][k] = WeightHO[0][k] ;
                 for( j = 1 ; j <= NumHidden ; j++ ) {
                     SumO[p][k] += Hidden[p][j] * WeightHO[j][k] ;
                 }
-                Output[p][k] = 1.0/(1.0 + exp(-SumO[p][k])) ;   /* Sigmoidal Outputs */
-//                Output[p][k] = SumO[p][k];    /*  Linear Outputs */
-                Error += 0.5 * (Target[p][k] - Output[p][k]) * (Target[p][k] - Output[p][k]) ;   /* SSE */
-//                Error -= ( Target[p][k] * log( Output[p][k] ) + ( 1.0 - Target[p][k] ) * log( 1.0 - Output[p][k] ) ) ;    /*Cross-Entropy Error */
-                DeltaO[k] = (Target[p][k] - Output[p][k]) * Output[p][k] * (1.0 - Output[p][k]) ;   /* Sigmoidal Outputs, SSE */
-//                DeltaO[k] = Target[p][k] - Output[p][k];    /* Sigmoidal Outputs, Cross-Entropy Error */
-//                DeltaO[k] = Target[p][k] - Output[p][k];    /* Linear Outputs, SSE */
+
+                Output[p][k] = 1.0/(1.0 + exp(-SumO[p][k])) ;   /* Sigmoidal Outputs VA BENE SOLO PER OUTPUT   1<=OUT<=0*/
+                Error -= ( allData[p].out[k] * log( Output[p][k] ) + ( 1.0 - allData[p].out[k] ) * log( 1.0 - Output[p][k] ) ) ;    /*Cross-Entropy Error UTILE PER PROBABILITY OUTPUT*/
+                DeltaO[k] = allData[p].out[k] - Output[p][k];    /* Sigmoidal Outputs, Cross-Entropy Error */
             }
 
-//#pragma omp for schedule reduction (+: SumDOW, DeltaH )
+
             for( j = 1 ; j <= NumHidden ; j++ ) {    /* 'back-propagate' errors to hidden layer */
                 SumDOW[j] = 0.0 ;
-//#pragma omp for schedule reduction (+: SumDOW)
-                for( k = 1 ; k <= NumOutput ; k++ ) {
+                for( k = 1 ; k <= numOut ; k++ ) {
                     SumDOW[j] += WeightHO[j][k] * DeltaO[k] ;
                 }
                 DeltaH[j] = SumDOW[j] * Hidden[p][j] * (1.0 - Hidden[p][j]) ;
             }
-
-//#pragma omp for schedule reduction (+: DeltaWeightIH, WeightIH )
             for( j = 1 ; j <= NumHidden ; j++ ) {     /* update weights WeightIH */
                 DeltaWeightIH[0][j] = eta * DeltaH[j] + alpha * DeltaWeightIH[0][j] ;
                 WeightIH[0][j] += DeltaWeightIH[0][j] ;
-//#pragma omp for schedule reduction (+: DeltaWeightIH, WeightIH )
-                for( i = 1 ; i <= NumInput ; i++ ) {
-                    DeltaWeightIH[i][j] = eta * Input[p][i] * DeltaH[j] + alpha * DeltaWeightIH[i][j];
+                for( i = 1 ; i <= numIn ; i++ ) {
+                    DeltaWeightIH[i][j] = eta * allData[p].in[i] * DeltaH[j] + alpha * DeltaWeightIH[i][j];
                     WeightIH[i][j] += DeltaWeightIH[i][j] ;
                 }
             }
-
-//#pragma omp for schedule reduction (+: DeltaWeightHO, WeightHO )
-            for( k = 1 ; k <= NumOutput ; k ++ ) {    /* update weights WeightHO */
+            for( k = 1 ; k <= numOut ; k ++ ) {    /* update weights WeightHO */
                 DeltaWeightHO[0][k] = eta * DeltaO[k] + alpha * DeltaWeightHO[0][k] ;
                 WeightHO[0][k] += DeltaWeightHO[0][k] ;
-//#pragma omp for schedule reduction (+: DeltaWeightHO, WeightHO )
                 for( j = 1 ; j <= NumHidden ; j++ ) {
                     DeltaWeightHO[j][k] = eta * Hidden[p][j] * DeltaO[k] + alpha * DeltaWeightHO[j][k] ;
                     WeightHO[j][k] += DeltaWeightHO[j][k] ;
                 }
             }
         }
-        if( epoch%100 == 0 ) fprintf(stdout, "\nEpoch %-5d :   Error = %f", epoch, Error) ;
-        if( Error < 0.00004 ) break ;  /* stop learning when 'near enough' */
+
+        accuracy=0;
+        sensitivity=0;
+
+        for(int pat=1;pat<numPattern;pat++) {
+            int class = 0;
+            for (int z = 1; z <= numOut; z++) {
+                if (allData[pat].out[z] < 1.1 && allData[pat].out[z] > 0.9){
+                    class=z;
+                }
+            }
+            int wrong=0;
+            double prob= Output[pat][class];
+            for (int z = 1; z <= numOut; z++) {
+                if (Output[pat][z] > prob && z!=class){
+                    wrong++;
+                }
+            }
+
+            if(wrong>0){
+                accuracy+=wrong;
+                sensitivity++;
+            }
+        }
+
+        accuracy=accuracy/(sensitivity);
+        sensitivity=(numPattern-sensitivity)/numPattern;
+
+
+        if(minAccuracy>accuracy)
+            minAccuracy=accuracy;
+
+        if(maxSensitivity<sensitivity)
+            maxSensitivity=sensitivity;
+
+        if( epoch%100 == 0 )
+            fprintf(stdout, "\nEpoch %-5d :   Error = %f\tminAcc=%-4f,\tmaxSens=%-4f,\t", epoch, Error/numPattern, accuracy, sensitivity) ;
+        //if( accuracy < (0.01) )
+        //break ;  /* stop learning when 'near enough' */
     }
 
-    fprintf(stdout, "\n\nNETWORK DATA - EPOCH %d\n\nPat\t", epoch) ;   /* print network outputs */
-    for( i = 1 ; i <= NumInput ; i++ ) {
+    fprintf(stdout, "\nminAcc=%-4f,\t", minAccuracy) ;
+    fprintf(stdout, "maxSens=%-4f", maxSensitivity) ;
+    //printf(stdout, "\n\nNETWORK DATA - EPOCH %d\n\nPat\t", epoch) ;   /* print network outputs */
+    /*for( i = 1 ; i <= numIn ; i++ ) {
         fprintf(stdout, "Input%-4d\t", i) ;
     }
-    for( k = 1 ; k <= NumOutput ; k++ ) {
+    for( k = 1 ; k <= numOut ; k++ ) {
         fprintf(stdout, "Target%-4d\tOutput%-4d\t", k, k) ;
-    }
-    for( p = 1 ; p <= NumPattern ; p++ ) {
+    }*/
+    for( p = 1 ; p <= 10 ; p++ ) {
         fprintf(stdout, "\n%d\t", p) ;
-        for( i = 1 ; i <= NumInput ; i++ ) {
-            fprintf(stdout, "%f\t", Input[p][i]) ;
-        }
-        for( k = 1 ; k <= NumOutput ; k++ ) {
-            fprintf(stdout, "%f\t%f\t", Target[p][k], Output[p][k]) ;
+        /*for( i = 1 ; i <= numIn ; i++ ) {
+            fprintf(stdout, "%f\t", allData[p].in[i]) ;
+        }*/
+        fprintf(stdout, "\n\n\n") ;
+        for( k = 1 ; k <= numOut ; k++ ) {
+            fprintf(stdout, "\n%f\t%f\t", allData[p].out[k], Output[p][k]) ;
         }
     }
-    fprintf(stdout, "\n\nGoodbye!\n\n") ;
+
+    for (int c=0;c<numPattern;c++){
+        free(allData[c].out);
+        free(allData[c].in);
+    }
+    free(allData);
+
     return 1 ;
 }
+
