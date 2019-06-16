b7e447885d886929167bd6763e607b8a1e7bf9fd
StefanoRicci94 <mr.ricci@hotmail.it>
2019-06-16 17:21:32 +0200

    Final Modify of Weights' manage, and adjust of SerialTest

--- main.c
+++ main.c
@@ -5,11 +5,13 @@
 #include "serialeTest.h"
 #include "parallel.h"
 #include "readData.h"
+#include "readInitialWeight.h"
 
 
 int main() {
     int numIn=11, numHid=5, numOut=1, numPat, numPatTest, epochMax=600000;
     double timeSeriale=0.0, timeParallel=0.0;
+    double **weightIH, **weightHO;
     char ans[20]="s";
 
     struct data *allData, *allDataTest;
@@ -19,25 +21,27 @@ int main() {
     char *fileNameDataTest="dataTest.csv";
     allDataTest=readData(numIn, numOut, &numPatTest, fileNameDataTest);
 
+
+    weightIH=readInitialWeightIH(numIn, numHid);
+    weightHO=readInitialWeightHO(numHid, numOut);
+
     printf("Enter number of epoch :\n");
     scanf("%d", &epochMax);
     printf("serial o parallel or all? s/p/a\n");
     //scanf("%s",ans);
 
     if(strcmp("a", ans)==0) {
-        double ***bestWeightSeriale = seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale);
+        seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale, weightIH, weightHO);
         double ***bestWeightParallel = parallel(allData, numIn, numHid, numOut, numPat, epochMax, &timeParallel);
 
-        free(bestWeightSeriale);
         free(bestWeightParallel);
 
         double time=timeSeriale-timeParallel;
         printf("\n\nt seriale=\t%.3lfs\nt parallelo=\t%.3lfs\ndifferenza=\t%.3lfs\n\n", timeSeriale, timeParallel, time);
 
     }else if(strcmp("s", ans)==0) {
-        double ***bestWeightSeriale = seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale);
-        serialeTest(allDataTest, numIn, numHid, numOut, numPatTest, bestWeightSeriale);
-        free(bestWeightSeriale);
+        seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale, weightIH, weightHO);
+        serialeTest(allDataTest, numIn, numHid, numOut, numPatTest, weightIH, weightHO);
 
         printf("\n\nt seriale=\t%.3lfs\n\n", timeSeriale);
     }else if(strcmp("p", ans)==0) {
