3bffaa2d03e453e87d8f9649a3095b81938c99d0
Marco <marco.prina01@universitadipavia.it>
2019-06-16 18:33:22 +0200

    Sdoppiati i weight

--- main.c
+++ main.c
@@ -11,7 +11,8 @@
 int main() {
     int numIn=11, numHid=5, numOut=1, numPat, numPatTest, epochMax=600000;
     double timeSeriale=0.0, timeParallel=0.0;
-    double **weightIH, **weightHO;
+    double **SweightIH, **SweightHO;
+    double **PweightIH, **PweightHO;
     char ans[20]="p";
 
     struct data *allData, *allDataTest;
@@ -21,30 +22,34 @@ int main() {
     char *fileNameDataTest="dataTest.csv";
     allDataTest=readData(numIn, numOut, &numPatTest, fileNameDataTest);
 
-
-    weightIH=readInitialWeightIH(numIn, numHid);
-    weightHO=readInitialWeightHO(numHid, numOut);
-
     printf("Enter number of epoch :\n");
     scanf("%d", &epochMax);
     printf("serial o parallel or all? s/p/a\n");
     //scanf("%s",ans);
 
     if(strcmp("a", ans)==0) {
-        seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale, weightIH, weightHO);
-        parallel(allData, numIn, numHid, numOut, numPat, epochMax, &timeParallel, weightIH, weightHO);
+        SweightIH=readInitialWeightIH(numIn, numHid);
+        SweightHO=readInitialWeightHO(numHid, numOut);
+        PweightIH=readInitialWeightIH(numIn, numHid);
+        PweightHO=readInitialWeightHO(numHid, numOut);
+        seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale, SweightIH, SweightHO);
+        parallel(allData, numIn, numHid, numOut, numPat, epochMax, &timeParallel, PweightIH, PweightHO);
 
         double time=timeSeriale-timeParallel;
         printf("\n\nt seriale=\t%.3lfs\nt parallelo=\t%.3lfs\ndifferenza=\t%.3lfs\n\n", timeSeriale, timeParallel, time);
 
     }else if(strcmp("s", ans)==0) {
-        seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale, weightIH, weightHO);
-        serialeTest(allDataTest, numIn, numHid, numOut, numPatTest, weightIH, weightHO);
+        SweightIH=readInitialWeightIH(numIn, numHid);
+        SweightHO=readInitialWeightHO(numHid, numOut);
+        seriale(allData, numIn, numHid, numOut, numPat, epochMax, &timeSeriale, SweightIH, SweightHO);
+        serialeTest(allDataTest, numIn, numHid, numOut, numPatTest, SweightIH, SweightHO);
 
         printf("\n\nt seriale=\t%.3lfs\n\n", timeSeriale);
     }else if(strcmp("p", ans)==0) {
-        parallel(allData, numIn, numHid, numOut, numPat, epochMax, &timeParallel, weightIH, weightHO);
-        serialeTest(allDataTest, numIn, numHid, numOut, numPatTest, weightIH, weightHO);
+        PweightIH=readInitialWeightIH(numIn, numHid);
+        PweightHO=readInitialWeightHO(numHid, numOut);
+        parallel(allData, numIn, numHid, numOut, numPat, epochMax, &timeParallel, PweightIH, PweightHO);
+        serialeTest(allDataTest, numIn, numHid, numOut, numPatTest, PweightIH, PweightHO);
 
         printf("\n\nt parallelo=\t%.3lfs\n\n", timeParallel);
     }
