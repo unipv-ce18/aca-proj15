a70e63ab2c72e7332fcf1ded1f1b6670846ee78b
Marco <marco.prina01@universitadipavia.it>
2019-06-16 22:20:08 +0200

    Risolto problema di overflow

--- readInitialWeight.c
+++ readInitialWeight.c
@@ -8,7 +8,7 @@ double** initialWeight(int row, int colomn,  char *fileName);
 
 double** readInitialWeightIH(int numIn, int numHid) {
     if(numIn>11 || numHid>29){
-        perror("Error, input is 11 and hidden is 17");
+        perror("Error max input is 11 and hidden is 29");
         exit(1);
     }
 
@@ -19,7 +19,7 @@ double** readInitialWeightIH(int numIn, int numHid) {
 
 double** readInitialWeightHO(int numHid, int numOut) {
     if(numHid>29 || numOut>30){
-        perror("Error, hidden max is 17 and out is 30");
+        perror("Error, max hidden is 17 and out is 30");
         exit(1);
     }
 
@@ -38,7 +38,7 @@ double** initialWeight(int row, int colomn,  char *fileName) {
 
 
     FILE *fd;
-    char buf[200];
+    char buf[500];
 
 
     fd=fopen(fileName, "r");
@@ -48,7 +48,7 @@ double** initialWeight(int row, int colomn,  char *fileName) {
     }
 
     for( int j = 0 ; j < row ; j++ ) {
-        if(fgets(buf, 200, fd)==NULL) {
+        if(fgets(buf, 500, fd)==NULL) {
             perror("Error, something got wrong");
             exit(1);
         }
